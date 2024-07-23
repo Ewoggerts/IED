0e3a131debd14229564622a0d400454096304085
ellazwu04 <ellazwu04@gmail.com>
2024-07-23 18:43:01 -0400

    Added Math Functions

---
+++ IED_code_2/Kinematics.ino
@@ -0,0 +1,26 @@
+#include <Math.h>
+
+float PI(){
+  float pi = 3.1415926535;
+  return pi;
+}
+
+
+// Converts input turn angle to the required number of wheel rotations to achieve that angle
+float turnAngleToWheelRev(float turnAngle, float driveBase, float wheelDiameter){
+  float turnCircumference = driveBase * PI();
+  float distPerWheelRev = wheelDiameter * PI();
+  float wheelRevPerFullRotation = turnCircumference / distPerWheelRev;
+  float degreeChangePerWheelRev = wheelRevPerFullRotation / 360;
+  float outputRevs = turnAngle * degreeChangePerWheelRev
+  return outputRevs;
+}
+
+// Converts input travel distance to the required number of wheel rotations to achieve that distance
+float distanceToWheelRev(float travelDistance, float wheelDiameter){
+  float travelDistancePerWheelRev = wheelDiameter * PI();
+  float outputRevs = travelDistance / travelDistancePerWheelRev;
+  return outputRevs;
+}
+
+float turn()
