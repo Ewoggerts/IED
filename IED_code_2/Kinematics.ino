9db218962357b9b8ad5de4025fab1d95aed36aa1
ellazwu04 <ellazwu04@gmail.com>
2024-07-23 19:05:38 -0400

    Update Kinematics.ino

--- IED_code_2/Kinematics.ino
+++ IED_code_2/Kinematics.ino
@@ -1,5 +1,6 @@
 #include <Math.h>
 
+
 float PI(){
   float pi = 3.1415926535;
   return pi;
@@ -7,20 +8,19 @@ float PI(){
 
 
 // Converts input turn angle to the required number of wheel rotations to achieve that angle
-float turnAngleToWheelRev(float turnAngle, float driveBase, float wheelDiameter){
+float turnAngleToWheelRev(float turnAngle, float driveBase, float wheelDiameter, int ticksPerRev){
   float turnCircumference = driveBase * PI();
   float distPerWheelRev = wheelDiameter * PI();
   float wheelRevPerFullRotation = turnCircumference / distPerWheelRev;
   float degreeChangePerWheelRev = wheelRevPerFullRotation / 360;
-  float outputRevs = turnAngle * degreeChangePerWheelRev
-  return outputRevs;
+  float outputRevs = turnAngle * degreeChangePerWheelRev * ticksPerRev;
+  return outputTicks;
 }
 
 // Converts input travel distance to the required number of wheel rotations to achieve that distance
-float distanceToWheelRev(float travelDistance, float wheelDiameter){
+float distanceToWheelRev(float travelDistance, float wheelDiameter, int ticksPerRev){
   float travelDistancePerWheelRev = wheelDiameter * PI();
-  float outputRevs = travelDistance / travelDistancePerWheelRev;
-  return outputRevs;
+  float outputRevs = travelDistance / travelDistancePerWheelRev *ticksPerRev;
+  return outputTicks;
 }
 
-float turn()
